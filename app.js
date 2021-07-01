35bdab82bfbd23c98669af063b507b93e7fe7eec
Jing Davies <78061103+jingd16@users.noreply.github.com>
2021-07-01 18:49:56 +1000

    upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 // Button functions
 var yaxis = 100
 function buttonGo() {
@@ -36,21 +38,21 @@ function buttonGo() {
     var trace1 = {
         x: ['Cost vs Profit'],
         y: [VolInc],
-        name: 'Volumn Increase',
+        name: 'Direct Cost',
         type: 'bar'
       };
       
       var trace2 = {
         x: ['Cost vs Profit'],
         y: [PriceInc],
-        name: 'Price Increase',
+        name: 'Overheads',
         type: 'bar'
       };
 
       var trace3 = {
         x: ['Cost vs Profit'],
         y: [DrCostDec],
-        name: 'Direct Cost Decrease',
+        name: 'Profit',
         type: 'bar'
       };
 
@@ -78,5 +80,60 @@ function buttonReset() {
     document.getElementById("outPriceInc").value = 0
     document.getElementById("outDrCostDec").value = 0
     document.getElementById("fixCostDec").value = 0
+
+    init();
     
+}
+
+function init() {
+  // ====== Revenue Bar Chart =======
+ var data = [
+  {
+    x: ["revenue"],
+    y: [100],
+    type: 'bar'
+  }
+];
+
+var layout = {
+  title: "Revenue",
+  height: 600,
+  width: 400
+};
+
+Plotly.newPlot("bar", data, layout);
+
+
+// =========Profit Bar Chart ======
+
+var trace1 = {
+  x: ['Cost vs Profit'],
+  y: [30],
+  name: 'Direct Cost',
+  type: 'bar'
+};
+
+var trace2 = {
+  x: ['Cost vs Profit'],
+  y: [60],
+  name: 'Overheads',
+  type: 'bar'
+};
+
+var trace3 = {
+  x: ['Cost vs Profit'],
+  y: [10],
+  name: 'Profit',
+  type: 'bar'
+};
+
+var data1 = [trace1, trace2, trace3];
+
+var layout1 = {barmode: 'stack',
+              title: "Cost vs Profit",
+              height: 600,
+              width: 400
+              };
+
+Plotly.newPlot('profitbar', data1, layout1);
 }
